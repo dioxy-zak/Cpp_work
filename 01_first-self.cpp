faa73a257e0fd345252dc2b23e7c47e2e9badb87
dioxy-zak <zakariamoustahib@gmail.com>
2026-09-02 15:28:44 +0800

    first one

---
+++ 01_first-self.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+int main(){
+    int myNumber = 2006;
+    char myLetter = 'Z';
+    std::cout << "Hello " << myNumber << " "<< myLetter;
+    std::cout <<" "<< "tst " << myNumber << " "<< myLetter<< std::endl;
+    std::cout <<"Hello " << myNumber << " "<< myLetter<< std::endl;
+    std::cout <<"tst " << myNumber << " "<< myLetter<< std::endl;
+    return 0;
+}
