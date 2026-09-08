898fdea1767fad09f3d311d3997df33b371f5bef
dioxy-zak <zakariamoustahib@gmail.com>
2026-09-08 22:00:08 +0800

    second one

---
+++ 02_sizes.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+int main() {
+    std::cout << "Size of char: " << sizeof(char) << " bytes" << std::endl;
+    std::cout << "Size of int: " << sizeof(int) << " bytes" << std::endl;
+    std::cout << "Size of float: " << sizeof(float) << " bytes" << std::endl;
+    std::cout << "Size of double: " << sizeof(double) << " bytes" << std::endl;
+    std::cout << "Size of bool: " << sizeof(bool) << " bytes" << std::endl;
+    std::cout << "Size of short int: " << sizeof(short int) << " bytes" << std::endl;
+    std::cout << "Size of long int: " << sizeof(long int) << " bytes" << std::endl;
+    std::cout << "Size of long double: " << sizeof(long double) << " bytes" << std::endl;
+    return 0;
+}
