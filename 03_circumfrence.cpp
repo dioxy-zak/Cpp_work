9dd71638b9a03f92b557affe7071e535f5adfefb
dioxy-zak <zakariamoustahib@gmail.com>
2026-09-09 14:24:25 +0800

    tireeeees

---
+++ 03_circumfrence.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#define PI 3.14159
+#define D1 17
+#define D2 19
+#define D3 21
+int main() {
+    std::cout << "Circumference of tire 1 = " << PI * D1 << std::endl;
+    std::cout << "Circumference of tire 2 = " << PI * D2 << std::endl;
+    std::cout << "Circumference of tire 3 = " << PI * D3 << std::endl;
+    return 0;
+}
