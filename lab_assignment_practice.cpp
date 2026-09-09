8bcbb11823941c30e10dbec1c25ec695e770d671
dioxy-zak <zakariamoustahib@gmail.com>
2026-09-09 15:36:28 +0800

    wednsday

---
+++ lab_assignment_practice.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main (){
+string sentence= "my exact age is: " ;
+int birthday = 2005;
+float age = 21.1;
+cout << "my birthday is "<<birthday<< " and "<<sentence<<age<<" years old."<< endl;
+return 0;
+
+}
